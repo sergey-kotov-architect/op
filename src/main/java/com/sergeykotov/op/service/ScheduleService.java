f67e26902240dd412e177d0239e7535660a10ce7
Sergey Kotov <sergey1kotov@protonmail.com>
2019-10-03 09:23:17 +0200

    fix schedule generation

--- src/main/java/com/sergeykotov/op/service/ScheduleService.java
+++ src/main/java/com/sergeykotov/op/service/ScheduleService.java
@@ -6,12 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class ScheduleService {
@@ -72,12 +68,13 @@ public class ScheduleService {
 
         log.info("generating a schedule...");
         long start = System.currentTimeMillis();
-        Stream<Op> scheduledOps = schedule(ops.stream());
+        List<Op> scheduledOps = schedule(ops);
         long elapsed = System.currentTimeMillis() - start;
         log.info("schedule has been generated");
 
-        log.info("saving " + scheduledOps.count() + " scheduled operations...");
-        boolean saved = opService.update(scheduledOps.peek(o -> o.setScheduled(true)).collect(Collectors.toList()));
+        log.info("saving " + scheduledOps.size() + " scheduled operations...");
+        List<Op> scheduled = scheduledOps.stream().peek(o -> o.setScheduled(true)).collect(Collectors.toList());
+        boolean saved = opService.update(scheduled);
         String result = saved ? "schedule has been generated" : "failed to generate a schedule";
         String note = result + ", elapsed " + elapsed + " milliseconds";
         if (saved) {
@@ -88,47 +85,49 @@ public class ScheduleService {
         return note;
     }
 
-    private Stream<Op> schedule(Stream<Op> ops) {
-        Stream<Stream<Op>> schedules = getSchedules(ops);
-        log.info("selecting an optimal schedule from " + schedules.count() + " possible ones");
+    private List<Op> schedule(List<Op> ops) {
+        List<List<Op>> schedules = getSchedules(ops);
+        System.out.println("selecting an optimal schedule from " + schedules.size() + " possible ones");
         return getOptimalSchedule(schedules);
     }
 
-    private Stream<Op> getOptimalSchedule(Stream<Stream<Op>> schedules) {
-        return schedules.min(Comparator.comparingDouble(this::getDeviation)).orElse(Stream.empty());
+    private List<Op> getOptimalSchedule(List<List<Op>> schedules) {
+        return schedules.stream().min(Comparator.comparingDouble(this::getDeviation)).orElse(Collections.emptyList());
     }
 
-    private double getDeviation(Stream<Op> ops) {
-        Set<Actor> actors = ops.map(Op::getActor).collect(Collectors.toSet());
+    private double getDeviation(List<Op> ops) {
+        Set<Actor> actors = ops.stream().map(Op::getActor).collect(Collectors.toSet());
         int actorCount = actors.size();
-        double meanOpCountPerActor = (double) ops.count() / actorCount;
+        double meanOpCountPerActor = (double) ops.size() / actorCount;
         double deviationSum = 0.0;
         for (Actor actor : actors) {
-            long opCountPerActor = ops.filter(o -> o.getActor().equals(actor)).count();
+            long opCountPerActor = ops.stream().filter(o -> o.getActor().equals(actor)).count();
             double deviation = Math.abs(opCountPerActor - meanOpCountPerActor);
             deviationSum += deviation;
         }
         return deviationSum / actorCount;
     }
 
-    private Stream<Stream<Op>> getSchedules(Stream<Op> ops) {
-        List<Stream<Op>> schedules = new ArrayList<>();
-        Set<OpType> opTypes = ops.map(Op::getOpType).collect(Collectors.toSet());
-        Set<LocalDate> dates = ops.map(Op::getDate).collect(Collectors.toSet());
+    private List<List<Op>> getSchedules(List<Op> ops) {
+        List<List<Op>> schedules = new ArrayList<>();
+        Set<OpType> opTypes = ops.stream().map(Op::getOpType).collect(Collectors.toSet());
+        Set<LocalDate> dates = ops.stream().map(Op::getDate).collect(Collectors.toSet());
         for (OpType opType : opTypes) {
             for (LocalDate date : dates) {
-                List<Op> opsForActor = ops
+                List<Op> opsForActor = ops.stream()
                         .filter(o -> o.getOpType().equals(opType) && o.getDate().equals(date))
                         .collect(Collectors.toList());
-                List<Stream<Op>> newSchedules = new ArrayList<>();
+                List<List<Op>> newSchedules = new ArrayList<>();
                 for (Op op : opsForActor) {
-                    for (Stream<Op> s : schedules) {
-                        newSchedules.add(Stream.concat(s, Stream.of(op)));
+                    for (List<Op> s : schedules) {
+                        List<Op> newSchedule = new ArrayList<>(s);
+                        newSchedule.add(op);
+                        newSchedules.add(newSchedule);
                     }
                 }
                 schedules = newSchedules;
             }
         }
-        return schedules.stream();
+        return schedules;
     }
 }
