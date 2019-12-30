65a21aaaa4f02da8f5ba5e7dacf32f1eafb8bbb2
Sergey Kotov <sergey1kotov@protonmail.com>
2019-12-30 20:58:15 +0100

    refactor exception handler

--- src/main/java/com/sergeykotov/op/exception/ControllerExceptionHandler.java
+++ src/main/java/com/sergeykotov/op/exception/ControllerExceptionHandler.java
@@ -8,38 +8,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+
     @ExceptionHandler(value = NotFoundException.class)
     public ResponseEntity<?> handleNotFoundException(NotFoundException exception) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @ResponseBody
+    @ExceptionHandler(value = InvalidDataException.class)
+    public ResponseEntity<?> handleInvalidDataException(InvalidDataException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+    }
+
+    @ResponseBody
     @ExceptionHandler(value = ModificationException.class)
     public ResponseEntity<?> handleModificationException(ModificationException exception) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body("schedule is being generated, please wait to modify data");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("schedule is being generated, please wait to modify");
     }
 
     @ResponseBody
     @ExceptionHandler(value = DatabaseException.class)
     public ResponseEntity<?> handleDatabaseException(DatabaseException exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("failed to modify the database: " + exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("failed to modify the database");
     }
 
     @ResponseBody
     @ExceptionHandler(value = ExtractionException.class)
     public ResponseEntity<?> handleExtractionException(ExtractionException exception) {
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("failed to extract data from the database: " + exception.getMessage());
-    }
-
-    @ResponseBody
-    @ExceptionHandler(value = InvalidDataException.class)
-    public ResponseEntity<?> handleInvalidDataException(InvalidDataException exception) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("failed to extract data from the database");
     }
 }
