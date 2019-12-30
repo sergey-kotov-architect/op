ec4c77751e424262eb8a1bc491e4fa0f3447ec17
Sergey Kotov <sergey1kotov@protonmail.com>
2019-12-30 20:43:59 +0100

    change JSON property in Op: opType following camel case notation according to Google JSON style guide

--- src/main/java/com/sergeykotov/op/domain/Op.java
+++ src/main/java/com/sergeykotov/op/domain/Op.java
@@ -1,7 +1,5 @@
 package com.sergeykotov.op.domain;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -64,7 +62,6 @@ public class Op {
         this.actor = actor;
     }
 
-    @JsonProperty("op_type")
     public OpType getOpType() {
         return opType;
     }
