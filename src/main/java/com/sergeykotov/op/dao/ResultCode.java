626a1cbfb5aab822405d58254f0bf9b75b8362a5
Sergey Kotov <sergey1kotov@protonmail.com>
2019-12-31 21:10:06 +0100

    refactoring

--- src/main/java/com/sergeykotov/op/dao/ResultCode.java
+++ src/main/java/com/sergeykotov/op/dao/ResultCode.java
@@ -3,13 +3,13 @@ package com.sergeykotov.op.dao;
 public enum ResultCode {
     SQLITE_CONSTRAINT(19, "Abort due to constraint violation");
 
+    private final int code;
+    private final String name;
+
     ResultCode(int code, String name) {
         this.code = code;
         this.name = name;
     }
-
-    private final int code;
-    private final String name;
 
     public int getCode() {
         return code;
